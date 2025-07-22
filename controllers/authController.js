e0706c45c62bca5f11fc4acf22415bb8d796fbe1
jtanman <jtanman@gmail.com>
2025-07-22 15:45:31 -0700

    Add sign-up form

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,21 @@
+// Handle POST /sign-up
+const { Pool } = require('pg');
+const encodedPw = encodeURIComponent(process.env.SUPABASE_PW);
+const pool = new Pool({
+  connectionString: `postgresql://postgres.bfuxedgtnsqidizgwtbk:${encodedPw}@aws-0-us-west-1.pooler.supabase.com:6543/postgres`,
+});
+
+exports.sign_up_post = async (req, res, next) => {
+  try {
+    await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
+      req.body.username,
+      req.body.password,
+    ]);
+    res.redirect("/");
+  } catch(err) {
+    return next(err);
+  }
+};
 // controllers/authController.js
 
 exports.sign_up_get = (req, res) => {
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,8 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
+
 router.get('/sign-up', authController.sign_up_get);
+router.post('/sign-up', authController.sign_up_post);
 
 module.exports = router;
